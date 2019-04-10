e4f22729d6e1c272309b5eb920a6fe9cec6db074
trinidadcisneros <33505150+trinidadcisneros@users.noreply.github.com>
2019-04-09 21:27:54 -0700

    Update occupational_db.sql

--- occupational_db.sql
+++ occupational_db.sql
@@ -4,7 +4,6 @@ USE tech_occupational_db;
 
 -- Create tables for raw data to be loaded into
 -- DROP TABLE IF EXISTS ccupations;
-DROP TABLE job_listings;
 DROP TABLE yearly_stats;
 DROP TABLE occupations;
 
@@ -19,19 +18,6 @@ CREATE TABLE IF NOT EXISTS yearly_stats (
   occupation_code VARCHAR(16),
   hourly_median FLOAT,
   annual_median FLOAT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (occupation_code) REFERENCES occupations(occupation_code)
-);
-
-CREATE TABLE IF NOT EXISTS job_listings (
-  id INT AUTO_INCREMENT,
-  occupation_code VARCHAR(16),
-  occupation_title VARCHAR(255),
-  result_title VARCHAR(255),
-  pay_range VARCHAR(255),
-  experience TEXT,
-  education TEXT,
-  location_ VARCHAR(255),
   PRIMARY KEY (id),
   FOREIGN KEY (occupation_code) REFERENCES occupations(occupation_code)
 );
